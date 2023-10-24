77c5919639ea6cdaeae9fbe84dcba52f25fba649
zaynabnour-alghosh <zeeghosh@gmail.com>
2023-10-24 17:50:10 +0300

    <web-app> -- client -- <> Added Blockly Workspace

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import './App.css';
+
 import Game from './Pages/Game';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>      
-        <Route path='/' element={<Game />} />
+   <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Game />}/>
       </Routes>
-    </BrowserRouter>
-  
+   </BrowserRouter>
   );
 }
 
--- web-client/src/Components/GameDesign/index.jsx
+++ web-client/src/Components/GameDesign/index.jsx
@@ -1,9 +1,41 @@
 import React from "react";
-const GameDesign =()=>{
-    return (
-        <div>
-            Game design component
-        </div>
-    );
+import './style.css';
+import { useState } from "react";
+import {BlocklyWorkspace} from "react-blockly";
+
+const GameDesign = () => {
+  const [xml, setXml] = useState();
+  const toolboxConfiguration = {
+    kind: "categoryToolbox",
+    contents: [
+      {
+        kind: "category",
+        name: "Logic",
+        colour: "#5C81A6",
+        contents: [
+          {
+            kind: "block",
+            type: "controls_if",
+          },
+          {
+            kind: "block",
+            type: "logic_compare",
+          },
+        ],
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <BlocklyWorkspace
+        className="work-space"
+        initialXml={xml}
+        onXmlChange={setXml}
+        toolboxConfiguration={toolboxConfiguration}
+      />
+    </div>
+  );
 };
+
 export default GameDesign;
